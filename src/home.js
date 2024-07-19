3851a502e588a1a0beeab55fac5ad8e39a980039
AYB <ayyubiy67@gmail.com>
2024-07-19 02:50:28 +0100

    Update image path and configure Webpack to handle images

--- src/home.js
+++ src/home.js
@@ -3,6 +3,6 @@ export default function loadHome() {
   content.innerHTML = `
     <h1>Welcome to Our Restaurant</h1>
     <p>We serve delicious food made from fresh ingredients!</p>
-    <img src="path_to_image" alt="Restaurant" style="width: 100%; height: auto;" />
+    <img src="./assets/food1.jpg" alt="Restaurant" style="width: 100%; height: auto;" />
   `;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,29 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/",
+            },
+          },
+        ],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
   ],
-  mode: "development",
 };
